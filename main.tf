1dfe76a6a75fbb39f444456cde23dbe8c73186c7
anuragsuraj23 <anurag.suraj23@gmail.com>
2025-02-12 14:57:36 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,34 +1,12 @@
-resource "aws_subnet" "private_subnet" {
-  vpc_id                  = data.aws_vpc.vpc.id
-  cidr_block              = "10.0.1.0/24"  # Change only the 3rd octet
-  map_public_ip_on_launch = false
-  availability_zone       = "ap-south-1a"
-
-  tags = {
-    Name = "private-subnet"
-  }
-}
-
-resource "aws_route_table" "private_rt" {
-  vpc_id = data.aws_vpc.vpc.id
-
-  route {
-    cidr_block = "0.0.0.0/0"
-    nat_gateway_id = data.aws_nat_gateway.nat.id
-  }
-
-  tags = {
-    Name = "private-route-table"
-  }
-}
-
-resource "aws_route_table_association" "private_association" {
-  subnet_id      = aws_subnet.private_subnet.id
-  route_table_id = aws_route_table.private_rt.id
-}
-
 resource "aws_security_group" "lambda_sg" {
   vpc_id = data.aws_vpc.vpc.id
+
+  ingress {
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 
   egress {
     from_port   = 0
@@ -36,19 +14,20 @@ resource "aws_security_group" "lambda_sg" {
     protocol    = "-1"
     cidr_blocks = ["0.0.0.0/0"]
   }
+}
 
-  tags = {
-    Name = "lambda-security-group"
-  }
+resource "aws_subnet" "private_subnet" {
+  vpc_id                  = data.aws_vpc.vpc.id
+  cidr_block              = "10.0.1.0/24"
+  map_public_ip_on_launch = false
 }
 
 resource "aws_lambda_function" "lambda_function" {
   function_name = "trigger-api-function"
   role          = data.aws_iam_role.lambda.arn
-  runtime       = "python3.9"
   handler       = "lambda_function.lambda_handler"
-
-  filename         = "lambda_payload.zip"  # This zip file should contain your Lambda function
+  runtime       = "python3.8"
+  filename      = "lambda_payload.zip"
   source_code_hash = filebase64sha256("lambda_payload.zip")
 
   vpc_config {
@@ -58,13 +37,7 @@ resource "aws_lambda_function" "lambda_function" {
 
   environment {
     variables = {
-      SUBNET_ID  = aws_subnet.private_subnet.id
-      FULL_NAME  = "Anurag Dangi"
-      EMAIL      = "anurag.suraj23@gmail.com"
+      API_URL = "https://bc1yy8dzsg.execute-api.eu-west-1.amazonaws.com/v1/data"
     }
-  }
-
-  tags = {
-    Name = "LambdaFunction"
   }
 }
