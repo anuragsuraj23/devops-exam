f10d3c327c566d32a8dd1c4a3c1d7a7c6c11d5c6
anuragsuraj23 <anurag.suraj23@gmail.com>
2025-02-12 14:57:57 +0530

    Update lambda_function.py

--- lambda_function.py
+++ lambda_function.py
@@ -1,20 +1,19 @@
 import json
-import os
 import requests
+import os
 
 def lambda_handler(event, context):
-    api_url = "https://bc1yy8dzsg.execute-api.eu-west-1.amazonaws.com/v1/data"
+    api_url = os.environ['API_URL']
     headers = {"X-Siemens-Auth": "test"}
-
     payload = {
-        "subnet_id": os.environ['SUBNET_ID'],
-        "full_name": os.environ['FULL_NAME'],
-        "email": os.environ['EMAIL']
+        "subnet_id": event["subnet_id"],
+        "full_name": event["full_name"],
+        "email": event["email"]
     }
 
     response = requests.post(api_url, json=payload, headers=headers)
-    
     return {
         "statusCode": response.status_code,
-        "body": response.text
+        "body": response.json()
     }
+}
