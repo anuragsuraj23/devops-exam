8933840a2241ba9f00fc5f1ff3956e4d65113006
anuragsuraj23 <anurag.suraj23@gmail.com>
2025-02-12 15:34:43 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,15 +1,19 @@
-resource "aws_vpc" "main" {
+provider "aws" {
+  region = "ap-south-1"
+}
+
+resource "aws_vpc" "my_vpc" {
   cidr_block = "10.0.0.0/16"
 }
 
-resource "aws_subnet" "private" {
-  vpc_id                  = aws_vpc.main.id
+resource "aws_subnet" "private_subnet" {
+  vpc_id                  = aws_vpc.my_vpc.id
   cidr_block              = "10.0.1.0/24"
   map_public_ip_on_launch = false
 }
 
 resource "aws_security_group" "lambda_sg" {
-  vpc_id = aws_vpc.main.id
+  vpc_id = aws_vpc.my_vpc.id
 
   ingress {
     from_port   = 443
@@ -17,15 +21,8 @@ resource "aws_security_group" "lambda_sg" {
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
-
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
 }
 
-output "private_subnet_id" {
-  value = aws_subnet.private.id
+output "subnet_id" {
+  value = aws_subnet.private_subnet.id
 }
