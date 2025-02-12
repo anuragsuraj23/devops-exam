947c402c8d324053365d3625b2f43245e3970456
anuragsuraj23 <anurag.suraj23@gmail.com>
2025-02-12 16:16:06 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,6 +1,6 @@
 resource "aws_subnet" "private" {
   vpc_id            = data.aws_vpc.vpc.id
-  cidr_block        = "10.0.2.0/24"  # <-- Fixed CIDR conflict
+  cidr_block        = "10.0.1.0/24"  
   availability_zone = "ap-south-1a"
   map_public_ip_on_launch = false
 }
