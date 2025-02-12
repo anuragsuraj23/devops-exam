dd39158d793f91c66f8a0f50b2a04d853bd69c2b
anuragsuraj23 <anurag.suraj23@gmail.com>
2025-02-12 16:05:32 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -1,11 +1,12 @@
 terraform {
   backend "s3" {
     bucket         = "467.devops.candidate.exam"
-    key           = "terraform.tfstate"
-    region        = "ap-south-1"
+    key            = "terraform.tfstate"
+    region         = "ap-south-1"
   }
 }
 
 provider "aws" {
   region = "ap-south-1"
 }
+
