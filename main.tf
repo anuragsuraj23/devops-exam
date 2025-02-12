140c3564e9d67c74dfb6619beaec197f87c26c72
anuragsuraj23 <anurag.suraj23@gmail.com>
2025-02-12 15:58:55 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,12 +1,15 @@
+resource "aws_subnet" "private" {
+  vpc_id            = data.aws_vpc.vpc.id
+  cidr_block        = "10.0.1.0/24"
+  availability_zone = "ap-south-1a"
+
+  tags = {
+    Name = "private-subnet"
+  }
+}
+
 resource "aws_security_group" "lambda_sg" {
   vpc_id = data.aws_vpc.vpc.id
-
-  ingress {
-    from_port   = 443
-    to_port     = 443
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
 
   egress {
     from_port   = 0
@@ -14,30 +17,8 @@ resource "aws_security_group" "lambda_sg" {
     protocol    = "-1"
     cidr_blocks = ["0.0.0.0/0"]
   }
-}
 
-resource "aws_subnet" "private_subnet" {
-  vpc_id                  = data.aws_vpc.vpc.id
-  cidr_block              = "10.0.1.0/24"
-  map_public_ip_on_launch = false
-}
-
-resource "aws_lambda_function" "lambda_function" {
-  function_name = "trigger-api-function"
-  role          = data.aws_iam_role.lambda.arn
-  handler       = "lambda_function.lambda_handler"
-  runtime       = "python3.8"
-  filename      = "lambda_payload.zip"
-  source_code_hash = filebase64sha256("lambda_payload.zip")
-
-  vpc_config {
-    subnet_ids         = [aws_subnet.private_subnet.id]
-    security_group_ids = [aws_security_group.lambda_sg.id]
-  }
-
-  environment {
-    variables = {
-      API_URL = "https://bc1yy8dzsg.execute-api.eu-west-1.amazonaws.com/v1/data"
-    }
+  tags = {
+    Name = "lambda-security-group"
   }
 }
