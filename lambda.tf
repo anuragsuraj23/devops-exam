ac7f683039b95840f70abf025401f0bed41953a8
anuragsuraj23 <anurag.suraj23@gmail.com>
2025-02-12 15:59:40 +0530

    Update lambda.tf

--- lambda.tf
+++ lambda.tf
@@ -1,12 +1,14 @@
 resource "aws_lambda_function" "my_lambda" {
-  function_name    = "my_lambda_function"
+  function_name    = "trigger-api"
   role            = data.aws_iam_role.lambda.arn
-  handler         = "lambda_function.lambda_handler"
-  runtime         = "python3.8"
-  filename        = "lambda_payload.zip"
-  source_code_hash = filebase64sha256("lambda_payload.zip")
+  handler        = "lambda_function.lambda_handler"
+  runtime        = "python3.9"
+  timeout        = 10
+
+  filename        = "lambda_function.zip"
+
   vpc_config {
-    subnet_ids         = [aws_subnet.private_subnet.id]
+    subnet_ids         = [aws_subnet.private.id]
     security_group_ids = [aws_security_group.lambda_sg.id]
   }
 }
