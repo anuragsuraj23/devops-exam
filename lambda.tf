1ee504cde2c1e921cd8077dad946b0d0c5551867
anuragsuraj23 <anurag.suraj23@gmail.com>
2025-02-12 15:18:01 +0530

    Update lambda.tf

--- lambda.tf
+++ lambda.tf
@@ -1,18 +1,21 @@
-resource "aws_lambda_function" "lambda" {
-  function_name = "MyLambdaFunction"
-  filename      = "lambda_payload.zip"
-  handler       = "lambda_function.lambda_handler"
-  runtime       = "python3.9"
-  role          = data.aws_iam_role.lambda.arn
-
-  vpc_config {
-    subnet_ids         = [data.aws_subnet.private_subnet.id]
-    security_group_ids = [aws_security_group.lambda_sg.id]
-  }
+resource "aws_lambda_function" "lambda_function" {
+  function_name    = "devops_lambda"
+  role            = data.aws_iam_role.lambda.arn
+  handler         = "lambda_function.lambda_handler"
+  runtime         = "python3.8"
+  filename        = "lambda_payload.zip"
+  source_code_hash = filebase64sha256("lambda_payload.zip")
 
   environment {
     variables = {
-      SUBNET_ID = data.aws_subnet.private_subnet.id
+      SUBNET_ID = aws_subnet.private_subnet.id
+      FULL_NAME = "Anurag Dangi"
+      EMAIL     = "anurag.suraj23@gmail.com"
     }
+  }
+
+  vpc_config {
+    subnet_ids         = [aws_subnet.private_subnet.id]
+    security_group_ids = [aws_security_group.lambda_sg.id]
   }
 }
