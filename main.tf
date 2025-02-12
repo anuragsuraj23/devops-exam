b937b66c28a47a15ab46b1da035f41b337c56513
anuragsuraj23 <anurag.suraj23@gmail.com>
2025-02-12 15:15:10 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,3 +1,28 @@
+terraform {
+  required_version = ">= 1.0.0"
+  backend "s3" {
+    bucket = "467.devops.candidate.exam"
+    region = "ap-south-1"
+    key    = "terraform/statefile"
+  }
+}
+
+provider "aws" {
+  region = "ap-south-1"
+}
+
+# Create a private subnet
+resource "aws_subnet" "private_subnet" {
+  vpc_id                  = data.aws_vpc.vpc.id
+  cidr_block              = "10.0.1.0/24"
+  availability_zone       = "ap-south-1a"
+  map_public_ip_on_launch = false
+  tags = {
+    Name = "private-subnet"
+  }
+}
+
+# Security group for Lambda
 resource "aws_security_group" "lambda_sg" {
   vpc_id = data.aws_vpc.vpc.id
 
@@ -14,30 +39,33 @@ resource "aws_security_group" "lambda_sg" {
     protocol    = "-1"
     cidr_blocks = ["0.0.0.0/0"]
   }
+
+  tags = {
+    Name = "lambda-sg"
+  }
 }
 
-resource "aws_subnet" "private_subnet" {
-  vpc_id                  = data.aws_vpc.vpc.id
-  cidr_block              = "10.0.1.0/24"
-  map_public_ip_on_launch = false
+# Route Table for Private Subnet
+resource "aws_route_table" "private_rt" {
+  vpc_id = data.aws_vpc.vpc.id
+
+  route {
+    cidr_block     = "0.0.0.0/0"
+    nat_gateway_id = data.aws_nat_gateway.nat.id
+  }
+
+  tags = {
+    Name = "private-route-table"
+  }
 }
 
-resource "aws_lambda_function" "lambda_function" {
-  function_name = "trigger-api-function"
-  role          = data.aws_iam_role.lambda.arn
-  handler       = "lambda_function.lambda_handler"
-  runtime       = "python3.8"
-  filename      = "lambda_payload.zip"
-  source_code_hash = filebase64sha256("lambda_payload.zip")
+# Associate Private Subnet with Route Table
+resource "aws_route_table_association" "private_assoc" {
+  subnet_id      = aws_subnet.private_subnet.id
+  route_table_id = aws_route_table.private_rt.id
+}
 
-  vpc_config {
-    subnet_ids         = [aws_subnet.private_subnet.id]
-    security_group_ids = [aws_security_group.lambda_sg.id]
-  }
-
-  environment {
-    variables = {
-      API_URL = "https://bc1yy8dzsg.execute-api.eu-west-1.amazonaws.com/v1/data"
-    }
-  }
+# Output Private Subnet ID for Lambda
+output "private_subnet_id" {
+  value = aws_subnet.private_subnet.id
 }
