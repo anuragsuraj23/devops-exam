aca71fe11db00df4897f577f59fb44d04d4d18ac
anuragsuraj23 <anurag.suraj23@gmail.com>
2025-02-12 15:25:33 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,30 +1,15 @@
-terraform {
-  required_version = ">= 1.0.0"
-  backend "s3" {
-    bucket = "467.devops.candidate.exam"
-    region = "ap-south-1"
-    key    = "terraform/statefile"
-  }
+resource "aws_vpc" "main" {
+  cidr_block = "10.0.0.0/16"
 }
 
-provider "aws" {
-  region = "ap-south-1"
-}
-
-# Create a private subnet
-resource "aws_subnet" "private_subnet" {
-  vpc_id                  = data.aws_vpc.vpc.id
+resource "aws_subnet" "private" {
+  vpc_id                  = aws_vpc.main.id
   cidr_block              = "10.0.1.0/24"
-  availability_zone       = "ap-south-1a"
   map_public_ip_on_launch = false
-  tags = {
-    Name = "private-subnet"
-  }
 }
 
-# Security group for Lambda
 resource "aws_security_group" "lambda_sg" {
-  vpc_id = data.aws_vpc.vpc.id
+  vpc_id = aws_vpc.main.id
 
   ingress {
     from_port   = 443
@@ -39,33 +24,8 @@ resource "aws_security_group" "lambda_sg" {
     protocol    = "-1"
     cidr_blocks = ["0.0.0.0/0"]
   }
-
-  tags = {
-    Name = "lambda-sg"
-  }
 }
 
-# Route Table for Private Subnet
-resource "aws_route_table" "private_rt" {
-  vpc_id = data.aws_vpc.vpc.id
-
-  route {
-    cidr_block     = "0.0.0.0/0"
-    nat_gateway_id = data.aws_nat_gateway.nat.id
-  }
-
-  tags = {
-    Name = "private-route-table"
-  }
-}
-
-# Associate Private Subnet with Route Table
-resource "aws_route_table_association" "private_assoc" {
-  subnet_id      = aws_subnet.private_subnet.id
-  route_table_id = aws_route_table.private_rt.id
-}
-
-# Output Private Subnet ID for Lambda
 output "private_subnet_id" {
-  value = aws_subnet.private_subnet.id
+  value = aws_subnet.private.id
 }
